03c681ee0fc04b8a4d4a2ec0746203c1434c1f9f
milltanya <tanya.miller.perm@gmail.com>
2021-05-10 19:02:49 +0300

    Add setLastLogEndDateTime endpoint

--- src/main/java/com/example/TimeTracker/controller/LogController.java
+++ src/main/java/com/example/TimeTracker/controller/LogController.java
@@ -6,13 +6,12 @@ import com.example.TimeTracker.model.Person;
 import com.example.TimeTracker.repository.LogsRepository;
 import com.example.TimeTracker.repository.PersonRepository;
 import com.example.TimeTracker.security.services.jwt.JwtUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -32,42 +31,64 @@ public class LogController {
     @Autowired
     PersonRepository personRepository;
 
-    private static final Logger logger = LoggerFactory.getLogger(LogController.class);
-
-
-    @PostMapping(value = "/add", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> addLog(@RequestBody LogRequest logRequest, @RequestHeader(value = "Authorization") String auth) {
-
+    private Person authorize(String auth) throws HttpClientErrorException {
         String token = auth.replace("Bearer ", "");
         if (!jwtUtils.validateJwtToken(token)) {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED);
         }
         String email = jwtUtils.getUserNameFromJwtToken(token);
         Optional<Person> user_optional = personRepository.findByEmail(email);
         if (user_optional.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
+        }
+        return user_optional.get();
+    };
+
+    @PostMapping(value = "/add", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> addLog(@RequestBody LogRequest logRequest, @RequestHeader(value = "Authorization") String auth) {
+
+        Person user;
+        try {
+            user = authorize(auth);
+        } catch (HttpClientErrorException e) {
+            return new ResponseEntity<>(e.getStatusCode());
         }
 
         DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
         LocalDateTime startDateTime = LocalDateTime.parse(logRequest.getStartDateTime(), formatter);
 
         Log log = Log.builder()
-            .user(user_optional.get())
-            .browser(logRequest.getBrowser())
-            .startDateTime(startDateTime)
-            .tabName(logRequest.getTabName())
-            .url(logRequest.getUrl())
-            .background(logRequest.getBackground())
-            .build();
+                .user(user)
+                .browser(logRequest.getBrowser())
+                .startDateTime(startDateTime)
+                .tabName(logRequest.getTabName())
+                .url(logRequest.getUrl())
+                .background(logRequest.getBackground())
+                .build();
 
         logsRepository.save(log);
+        return ResponseEntity.ok("");
+    }
 
-        Log prevLog = logsRepository
-                .findFirstByUserAndStartDateTimeBeforeOrderByStartDateTimeDesc(user_optional.get(), startDateTime);
+    @PostMapping(value = "/setLastLogEndDateTime", consumes = MediaType.TEXT_PLAIN_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> setLastLogEndDateTime(@RequestBody String datetime, @RequestHeader(value = "Authorization") String auth) {
 
-        if (prevLog != null) {
-            prevLog.setEndDateTime(startDateTime);
-            logsRepository.save(prevLog);
+        Person user;
+        try {
+            user = authorize(auth);
+        } catch (HttpClientErrorException e) {
+            return new ResponseEntity<>(e.getStatusCode());
+        }
+
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+        LocalDateTime endDateTime = LocalDateTime.parse(datetime, formatter);
+
+        Log log = logsRepository
+                .findFirstByUserAndStartDateTimeBeforeOrderByStartDateTimeDesc(user, endDateTime);
+
+        if (log != null && log.getEndDateTime() == null) {
+            log.setEndDateTime(endDateTime);
+            logsRepository.save(log);
         }
         return ResponseEntity.ok("");
     }
